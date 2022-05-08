1e666f804708bc5dfb19617b39c3e2e8f978a410
Emilia Aldana <46900954+Zuleta84@users.noreply.github.com>
2022-05-08 14:17:19 +0200

    Delete Main.hs

--- Main.hs
+++
@@ -1,9 +0,0 @@
-import Application () -- for YesodDispatch instance
-import Foundation
-import Yesod.Core
-import Yesod.Static
-
-main :: IO ()
-main = do
-  static@(Static settings) <- static "static"
-  warp 3000 $ App static
