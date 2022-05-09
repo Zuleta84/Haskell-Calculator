49355ec3d697d1d55cfa6aed49cabe025da28db4
Emilia Aldana <46900954+Zuleta84@users.noreply.github.com>
2022-05-09 21:43:17 +0200

    Text

--- Haskell Calculator/Settings/StaticFiles.hs
+++ Haskell Calculator/Settings/StaticFiles.hs
@@ -5,7 +5,7 @@ import Yesod.Static (staticFiles)
 
 -- This generates easy references to files in the static directory at compile time,
 -- giving you compile-time verification that referenced files exist.
--- Warning: any files added to your static directory during run-time can't be
+-- Any files added to your static directory during run-time can't be
 -- accessed this way. You'll have to use their FilePath or URL to access them.
 --
 -- For example, to refer to @static/js/script.js@ via an identifier, you'd use:
